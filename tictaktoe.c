d142423847aea0cf0533a23871252839d3b305f3
shane green <shanegreen@cpp.edu>
2022-02-03 22:18:22 -0800

    diagonal and tie not implemented

--- tictaktoe.c
+++ tictaktoe.c
@@ -20,21 +20,28 @@ int checkWin(char board[SIZE][SIZE]) {
     int crossOne = board[0][0]+board[1][1]+board[2][2];
     int crossTwo = board[0][2]+board[1][1]+board[2][0];
     int win = 0;
+
+    //this loop determins if a row is a win
     for (int i = 0; i < 3; i++) {
         charVal = board[i][0]+board[i][1]+board[i][2];
         if (charVal == 264 || charVal == 237) {
             win = 1;
         }
     }
+    //this loop determins if a col is a win
     for (int j = 0; j < 3; j++) {
         charVal = board[0][j]+board[1][j]+board[2][j];
         if (charVal == 264 || charVal == 237) {
             win = 1;
         }
     }
+    //checks cross
+    printf("crossOne : %d\n", crossOne);
     if (crossOne == 264 || crossOne == 237) {
         win = 1;
     }
+    //checks cross
+    printf("crossTwo : %d\n", crossTwo);
     if (crossTwo == 264 || crossTwo == 237) {
         win = 1;
     }
@@ -45,7 +52,10 @@ int checkWin(char board[SIZE][SIZE]) {
 int main() {
     //instance variables
     char board[SIZE][SIZE] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+    //booleans
     int win = 0;
+    int misclick = 0;
+    //counters
     int turn = 1;
     char let = 'X';
     char row;
@@ -56,7 +66,7 @@ int main() {
     //game loop, resolves once there is a winner
     while (win != 1) {
         printBoard(board);
-        printf("TURN\n %d", turn);
+        printf("TURN %d\n", turn);
         printf("Enter row. T, M, B: \n");
         scanf(" %c", &row);
         printf("Enter col. L, M, R: \n");
@@ -67,13 +77,31 @@ int main() {
         case 't':
             switch (col) {
                 case 'l':
-                    board[0][0] = let;
+                    if (board[0][0] != 'X' && board[0][0] != 'Y') {
+                        board[0][0] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                     break;
                 case 'm':
-                    board[0][1] = let;
+                    if (board[0][1] != 'X' && board[0][1] != 'Y') {
+                        board[0][1] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                     break;
                 case 'r':
-                    board[0][2] = let;
+                    if (board[0][2] != 'X' && board[0][2] != 'Y') {
+                        board[0][2] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                     break;
                 default:
                     printf("error");
@@ -83,14 +111,31 @@ int main() {
         case 'm':
             switch (col) {
                 case 'l':
-                    board[1][0] = let;
+                    if (board[1][0] != 'X' && board[1][0] != 'Y') {
+                        board[1][0] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                     break;
                 case 'm':
-                    board[1][1] = let;
+                    if (board[1][1] != 'X' && board[1][1] != 'Y') {
+                        board[1][1] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                     break;
                 case 'r':
-                    board[1][2] = let;
-                    break;
+                    if (board[1][2] != 'X' && board[1][2] != 'Y') {
+                        board[1][2] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                 default:
                     printf("error");
                     break;
@@ -99,14 +144,29 @@ int main() {
         case 'b':
             switch (col) {
                 case 'l':
-                    board[2][0] = let;
-                    break;
+                    if (board[2][0] != 'X' && board[2][0] != 'Y') {
+                        board[2][0] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                 case 'm':
-                    board[2][1] = let;
-                    break;
+                    if (board[2][1] != 'X' && board[2][1] != 'Y') {
+                        board[2][1] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                 case 'r':
-                    board[2][2] = let;
-                    break;
+                    if (board[2][2] != 'X' && board[2][2] != 'Y') {
+                        board[2][2] = let;
+                    } else {
+                        printf("space taken! try again\n");
+                        misclick = 1;
+                        break;
+                    }
                 default:
                     printf("error");
                     break;
@@ -118,14 +178,18 @@ int main() {
             break;
         }
 
-        if (let == 'X') {
-            let = 'Y';
+        if (misclick == 0) {
+            if (let == 'X') {
+                let = 'Y';
+            } else {
+                let = 'X';
+            }
+            turn++;
+            win = checkWin(board);
+            printf("win: %d\n", win);
         } else {
-            let = 'X';
+            misclick = 0;
         }
-        turn++;
-        win = checkWin(board);
-        printf("win: %d\n", win);
     }
     return 0;
 }
